f4ef45e028bb0bc7480cb41614a1fc5040f54486
Angel-Adroja <adrojaangel07@gmail.com>
2026-07-01 09:54:23 +0530

    Add files via upload

---
+++ project/roll/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Student
+# Create your views here.
+def home(request):
+    search=request.GET.get('search')
+    if search:
+        students=Student.objects.filter(name_icontains=search)
+    else:
+        students=Student.objects.all()
+                  
+    return render(request, 'home.html',{'students':students})
+
+def welcome(request):
+    return render(request, 'welcome.html')
